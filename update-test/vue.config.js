aa632c673487d085c21cb192ab315f951c02f6c9
Sasaki Syouta <syouta.sasaki+GitAccount@kitalive.co.jp>
2022-01-11 16:46:08 +0900

    v0.1.2

---
+++ update-test/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    pluginOptions: {
+      electronBuilder: {
+        builderOptions: {
+            publish: {
+                provider: "github",
+                owner: "syouta-sasaki-test",
+                repo: "electron-update-test"
+            }
+        }
+      }
+    }
+  }
