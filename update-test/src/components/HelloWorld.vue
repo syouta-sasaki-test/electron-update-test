15f118f0dc0274b74d66c3fa74603885866e21c6
Sasaki Syouta <syouta.sasaki+GitAccount@kitalive.co.jp>
2022-01-12 10:52:41 +0900

    v0.2.0

--- update-test/src/components/HelloWorld.vue
+++ update-test/src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      ddddddddddddddddddddddddddddd
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
--- update-test/vue.config.js
+++ update-test/vue.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-    pluginOptions: {
-      electronBuilder: {
-        builderOptions: {
-            publish: {
-                provider: "github",
-                owner: "syouta-sasaki-test",
-                repo: "electron-update-test"
-            }
-        }
+  publicPath: process.env.NODE_ENV === 'production' ? '/electron-update-test/' : '/',
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+          publish: {
+              provider: "github",
+              owner: "syouta-sasaki-test",
+              repo: "electron-update-test",
+              token: "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",
+              releaseType: "draft"
+          }
       }
     }
   }
+}
