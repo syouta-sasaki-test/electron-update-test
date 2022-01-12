dc82ad846937136933f0f64145ebeff38b848a73
Sasaki Syouta <syouta.sasaki+GitAccount@kitalive.co.jp>
2022-01-12 15:52:58 +0900

    v0.4.0

--- update-test/src/components/HelloWorld.vue
+++ update-test/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>_______________</p>
+    <p>@@@@@@@@@@@@@@@@@@</p>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
